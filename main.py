8cf92eca52e538ffefd1343cd82cf4a458c6c26a
VE ZA <36743311+fckveza@users.noreply.github.com>
2020-08-20 22:26:02 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,25 @@ def ytdl(url):
     }
     data = requests.get(BaseUrl+"/ytdl", params=params).json()
     return data
-  
 #example youtube download
 print(ytdl("https://youtu.be/IYNRADB6GP0"))
+
+def tiktokdl(url):
+    params = {
+      "apikey": Apikey, 
+      "link": url
+    }
+    data = requests.get(BaseUrl+"/tiktokdl", params=params).json()
+    return data
+#example tiktok download
+print(tiktokdl("https://vt.tiktok.com/yqyjPX/"))
+
+def timelinedl(url):
+    params = {
+      "apikey": Apikey, 
+      "link": url
+    }
+    data = requests.get(BaseUrl+"/timeline", params=params).json()
+    return data
+#example timeline download
+print(tiktokdl("https:/timeline.line.me/post/_dcAJhbTlVNqvq27gmoe1M6crJ5Xl1a9fUKXRyvw/1157692114207018096"))
